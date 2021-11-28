21870003c1817c2c3a382c40a4b7a42923c7f5c6
Bartłomiej Martenka <94762027+BarthazZ@users.noreply.github.com>
2021-11-28 14:32:28 +0100

    Dodaj pliki projektów.

---
+++ Task 1 Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_1_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A: Display all integers between 1 and 100 by using the while loop.");
+            int a = 1;
+            while (a <= 100)
+            {
+
+                Console.WriteLine(a);
+                a++;
+            }
+
+            Console.WriteLine("B: Display all integers between 2 and 1000 by using the for loop.");
+
+            for (int b = 2; b <= 1000; b += 2)
+            {
+
+                Console.WriteLine(b);
+
+            }
+        }
+    }
+}
+//List all integers between 1 and 100 by using the while loop.
